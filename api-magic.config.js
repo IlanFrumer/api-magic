c84f480f199ef0576221747645cb183423aaa7c3
Ilan Frumer <ilanfrumer@gmail.com>
2022-07-28 17:09:59 +0300

    add support for auth

--- api-magic.config.js
+++ api-magic.config.js
@@ -1,14 +1,18 @@
 // @ts-check
 require('dotenv/config');
 
-/** @type {import('./src/generate').GeneratorOptions} */
+/** @type {import('./src').GeneratorOptions} */
 const options = {
   output: 'test/',
   api: [
     {
       container: 'test',
-      host: process.env.API_URL,
-      swagger: '/api-json',
+      host: process.env.API_URL ?? '',
+      swagger: '/swagger/v1/swagger.json',
+      auth: {
+        username: process.env.API_USERNAME ?? '',
+        password: process.env.API_PASSWORD ?? '',
+      },
     },
   ],
 };
